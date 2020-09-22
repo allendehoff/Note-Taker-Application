66aff62ae8c4c0cd1a70b7ff51a5e47caa6cabab
Allen DeHoff <allen.dehoff21@gmail.com>
2020-09-22 09:00:10 -0600

    buttons works to got to notes.html and back to index.html

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -21,9 +21,14 @@ const notes = []
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/notes.html"));
 });
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/index.html"));
+});
+
 
 // Starts the server to begin listening
 // =============================================================
